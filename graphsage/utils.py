c278a556e6b4fa5f65585991589c18c6bf8c05b1
Hoang NT <hoangnt.titech@gmail.com>
2019-02-14 16:35:25 +0900

    remove constraint on networkx version

--- graphsage/utils.py
+++ graphsage/utils.py
@@ -11,7 +11,7 @@ from networkx.readwrite import json_graph
 version_info = list(map(int, nx.__version__.split('.')))
 major = version_info[0]
 minor = version_info[1]
-assert (major <= 1) and (minor <= 11), "networkx major version > 1.11"
+# assert (major <= 1) and (minor <= 11), "networkx major version > 1.11"
 
 WALK_LEN=5
 N_WALKS=50
