7b43835d4cab0156984a5405a9b38e92bbbc8254
gear <hoangnt.titech@gmail.com>
2019-02-17 18:09:51 +0900

    remove old run script

--- ppi_supervised_swap_2.sh
+++
@@ -1,1 +0,0 @@
-python -m graphsage.supervised_train --train_prefix ~/data/ppi/ppi --feats_suffix "random_swap_0.2" --model graphsage_maxpool --model_size small --sigmoid
--- ppi_supervised_swap_all.sh
+++
@@ -1,1 +0,0 @@
-python -m graphsage.supervised_train --train_prefix ~/data/ppi/ppi --feats_suffix "random_swap_$1" --model graphsage_maxpool --model_size small --sigmoid
