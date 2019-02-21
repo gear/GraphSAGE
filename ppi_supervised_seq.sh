a1bf0eb7145072e68c104547df786f9fc63282b8
Hoang NT <hoangnt.titech@gmail.com>
2019-02-21 10:16:52 +0900

    scripts for new experiments

---
+++ ppi_supervised_seq.sh
@@ -0,0 +1,5 @@
+python -m graphsage.supervised_train \
+       --train_prefix ~/data/ppi/ppi \
+       --model graphsage_seq \
+       --model_size big \
+       --sigmoid
---
+++ reddit_supervised.sh
@@ -0,0 +1,1 @@
+python -m graphsage.supervised_train --train_prefix ~/data/reddit/reddit --model graphsage_maxpool --model_size small --sigmoid
